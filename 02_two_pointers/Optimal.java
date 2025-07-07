c389e1e624f991b9dbe1232e726fc9ef291fd6aa
DevJoshi29 <etlocomotin@gmail.com>
2025-07-07 13:44:10 +0100

    added solution to LC 167 - Two Sum II

--- 02_two_pointers/Optimal.java
+++ 02_two_pointers/Optimal.java
@@ -3,12 +3,14 @@ class Solution {
       int i = 0; // left pointer
       int j = numbers.length - 1; // right pointer
 
+      // If the sum in our window is less/greater than the target we move either of the pointers as needed to find the two indexes
       while(i < j){
         if (numbers[i] + numbers[j] < target){
             i++;
         } else if (numbers[i] + numbers [j] > target){
             j--;
         } else{
+            // once we find target sum, we return the indexes and add 1 (as mentioned in the problem)
             return new int[]{i + 1, j + 1};
         }
       }
